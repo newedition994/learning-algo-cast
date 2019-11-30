75d94ba8b1ba5be489057e860dc86b3bc81299d0
Breyanna Vass <breyannavass@gmail.com>
2019-11-30 18:25:04 -0500

    debugged the exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,10 +7,11 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split("").reduce((reversed, character) => {
-    return character + reversed;
-  }, "");
+  //debugger;
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
+
+//reverse("asdf");
 
 module.exports = reverse;
 
