128f9f5d570ade50d6c217664c0ce53fef6123f1
Breyanna Vass <breyannavass@gmail.com>
2019-11-30 23:04:41 -0500

    passed the tests for reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     const reversed = n
+//       .toString()
+//       .split("")
+//       .reverse()
+//       .join("");
+
+//     if (n < 0) {
+//       return parseInt(reversed) * -1;
+//     }
+
+//     return parseInt(reversed);
+//   }
